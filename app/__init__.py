c9b8ce9b21ca0b94db448d83132295f161839e91
Kaleb <kaleb.grabast@my.utsa.edu>
2026-03-28 13:06:02 -0500

    add init

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
-placeholder
+from flask import Flask
+
+def create_app():
+  app = Flask(__name__)
+
+  #import routes
+  from .dashboard.routes import main
+  app.register_blueprint(main)
+
+  return app
